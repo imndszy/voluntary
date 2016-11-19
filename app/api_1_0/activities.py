1afb5c650ddd6602684600427221bd9710fa84f8
szy <shizhenyu96@gmail.com>
2016-11-19 14:22:45 +0800

    push activities test

--- app/api_1_0/activities.py
+++ app/api_1_0/activities.py
@@ -1,10 +1,15 @@
 # -*- coding:utf8 -*-
 # Author: shizhenyu96@gamil.com
 # github: https://github.com/imndszy
+from flask import request, jsonify
 from app.api_1_0 import api
 from app.admin.functions import admin_login_required
+from app import db
+from app.models import Unfinished_activity
 
-@api.route('/activities')
+@api.route('/activities', methods=['POST'])
 @admin_login_required
 def activity():
-    return 'a'
+    data = request.values
+    print data
+    return jsonify(status='ok')
