3671b750847d8cc2a95656ed9ca1520a7312cb57
szy <shizhenyu96@gmail.com>
2016-11-19 14:04:22 +0800

    admin login ok

--- app/api_1_0/activities.py
+++ app/api_1_0/activities.py
@@ -2,7 +2,9 @@
 # Author: shizhenyu96@gamil.com
 # github: https://github.com/imndszy
 from app.api_1_0 import api
+from app.admin.functions import admin_login_required
 
-@api.route('/activity')
+@api.route('/activities')
+@admin_login_required
 def activity():
-    pass
+    return 'a'
