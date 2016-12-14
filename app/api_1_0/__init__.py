21ca539374a4ee2ade5f7f1345636fb0d9895278
szy <shizhenyu96@gmail.com>
2016-12-14 17:50:53 +0800

    information out

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -5,4 +5,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import activities, admin, users, create_qrcode
+from . import activities, admin, users, create_qrcode, get_info
